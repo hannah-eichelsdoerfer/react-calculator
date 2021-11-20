97c1b92c42a1c41f157a5b926ccc3f406118dd41
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-11-21 00:19:33 +0100

    styling

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="calculator">
+        <div className="display">0</div>
+        <button>C</button>
+        <button>DEL</button>
+        <button>%</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button className="span-two">0</button>
+        <button>,</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
