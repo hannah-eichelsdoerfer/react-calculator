53fa72ec61c0b3e46ee863fcba31fe17aeb551f2
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-11-21 01:53:11 +0100

    functioning calculator - with eval

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, settext] = useState("");
+
+  const operators = ["/", "*", "-", "+"];
+
+  const handleClick = (e) => {
+    if (operators.includes(e.target.innerText)) {
+      if (text.slice(-1) !== e.target.innerText) {
+        settext(text + e.target.innerText);
+      }
+    } else {
+      settext(text + e.target.innerText);
+    }
+  };
+
+  const clear = () => {
+    settext("");
+  };
+
+  const deleteLast = () => {
+    settext(text.slice(0, -1));
+  };
+
+  const calculate = () => {
+    settext(eval(text).toString());
+  };
+
   return (
     <div className="container">
       <div className="calculator">
-        <div className="display">0</div>
-        <button>C</button>
-        <button>DEL</button>
-        <button>%</button>
-        <button>/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
+        <div className="display">{text === "" ? 0 : text}</div>
+        <button onClick={clear}>C</button>
+        <button onClick={deleteLast}>DEL</button>
+        <button onClick={handleClick}>%</button>
+        <button onClick={handleClick}>/</button>
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>*</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>-</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>+</button>
         <button className="span-two">0</button>
-        <button>,</button>
-        <button>=</button>
+        <button onClick={handleClick}>,</button>
+        <button onClick={calculate}>=</button>
       </div>
     </div>
   );
