f77850578c8150df1eac14731d3a7d0e6515262e
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-11-21 03:34:48 +0100

    fully functioning calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,81 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [text, settext] = useState("");
+const operators = ["/", "*", "-", "+"];
 
-  const operators = ["/", "*", "-", "+"];
+function App() {
+  const [display, setDisplay] = useState("");
+
+  const lastInput = display.slice(-1);
+  const lastInputWasOperator = operators.includes(lastInput);
+
+  const values = display.split(/([/*+-])/);
+  let lastDigits = values[values.length - 1];
 
   const handleClick = (e) => {
-    if (operators.includes(e.target.innerText)) {
-      if (text.slice(-1) !== e.target.innerText) {
-        settext(text + e.target.innerText);
-      }
-    } else {
-      settext(text + e.target.innerText);
-    }
+    setDisplay(display + e.target.innerText);
+  };
+
+  const handleClickOperator = (e) => {
+    !lastInputWasOperator && setDisplay(display + e.target.innerText);
   };
 
   const clear = () => {
-    settext("");
+    setDisplay("");
   };
 
   const deleteLast = () => {
-    settext(text.slice(0, -1));
+    setDisplay(display.slice(0, -1));
   };
 
   const calculate = () => {
-    settext(eval(text).toString());
+    try {
+      setDisplay(eval(display).toString());
+    } catch (error) {
+      console.log("Not a valid operation!");
+    }
+  };
+  const percentage = () => {
+    if (!lastInputWasOperator) {
+      values[values.length - 1] = parseFloat(lastDigits) / 100;
+      setDisplay(values.join(""));
+    }
+  };
+  const decimalSeperator = (e) => {
+    if (!lastDigits.includes(".")) {
+      setDisplay(display + e.target.innerText);
+    }
+  };
+  const zeroCheck = (e) => {
+    if (lastDigits.slice(-1) !== "0" || lastDigits.includes(".")) {
+      setDisplay(display + e.target.innerText);
+    }
   };
 
   return (
     <div className="container">
       <div className="calculator">
-        <div className="display">{text === "" ? 0 : text}</div>
+        <div className="display">{display === "" ? 0 : display}</div>
         <button onClick={clear}>C</button>
-        <button onClick={deleteLast}>DEL</button>
-        <button onClick={handleClick}>%</button>
-        <button onClick={handleClick}>/</button>
+        <button onClick={deleteLast}>⬅</button>
+        <button onClick={percentage}>%</button>
+        <button onClick={handleClickOperator}>/</button>
         <button onClick={handleClick}>7</button>
         <button onClick={handleClick}>8</button>
         <button onClick={handleClick}>9</button>
-        <button onClick={handleClick}>*</button>
+        <button onClick={handleClickOperator}>*</button>
         <button onClick={handleClick}>4</button>
         <button onClick={handleClick}>5</button>
         <button onClick={handleClick}>6</button>
-        <button onClick={handleClick}>-</button>
+        <button onClick={handleClickOperator}>-</button>
         <button onClick={handleClick}>1</button>
         <button onClick={handleClick}>2</button>
         <button onClick={handleClick}>3</button>
-        <button onClick={handleClick}>+</button>
-        <button className="span-two">0</button>
-        <button onClick={handleClick}>,</button>
+        <button onClick={handleClickOperator}>+</button>
+        <button onClick={zeroCheck} className="span-two">
+          0
+        </button>
+        <button onClick={decimalSeperator}>.</button>
         <button onClick={calculate}>=</button>
       </div>
     </div>
